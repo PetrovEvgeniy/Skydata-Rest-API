2497b07162d033886e84c1eeb5aa7208e22d819a
PetrovEvgeniy <evgeniypetrov945@gmail.com>
2020-04-18 23:10:25 +0300

    fixed error

--- config/config.js
+++ config/config.js
@@ -3,7 +3,7 @@ const env = process.env.NODE_ENV || 'development';
 const config = {
     development: {
         port: process.env.PORT || 9999,
-        dbURL: 'mongodb://localhost:27017/skydata-db',
+        dbURL: process.env.MONGODB_URI,
         authCookieName: 'x-auth-token'
     },
     production: {
