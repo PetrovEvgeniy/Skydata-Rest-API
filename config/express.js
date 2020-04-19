6a1e120d466b2238a68700e670eb1f6245c19c90
PetrovEvgeniy <evgeniypetrov945@gmail.com>
2020-04-19 15:10:15 +0300

    test login issue

--- config/express.js
+++ config/express.js
@@ -6,7 +6,7 @@ const secret = 'ultrasecretsecret';
 
 module.exports = (app) => {
   app.use(cors({
-    origin: 'http://localhost:3000/',
+    origin: 'http://localhost:3000',
     credentials: true
   }));
 
