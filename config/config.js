f4a8abaa6b54efb85695d772cbd7b877d8d8061f
Evgeniy Petrov <37136002+PetrovEvgeniy@users.noreply.github.com>
2023-02-17 19:01:52 +0200

    Updated the configured cors URL

--- config/config.js
+++ config/config.js
@@ -4,7 +4,7 @@ const config = {
     development: {
         port: process.env.PORT || 9999,
         dbURL: 'mongodb+srv://admin-skydata:123321@skydata-project-cluster-gm2gy.mongodb.net/skydata-db?retryWrites=true&w=majority',
-	corsURL: 'http://localhost:3000',
+	corsURL: 'https://skydata-api.netlify.app/',
         authCookieName: 'x-auth-token'
     },
     production: {
