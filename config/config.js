1a1926447f16b6e4001ea33a8c8f5795cdb6d988
PetrovEvgeniy <evgeniypetrov945@gmail.com>
2023-03-01 23:50:49 +0100

    Update config file dbURL production

--- config/config.js
+++ config/config.js
@@ -9,7 +9,7 @@ const config = {
     },
     production: {
         port: process.env.PORT,
-        dbURL: process.env.MONGODB_URI,
+        dbURL: 'mongodb+srv://admin-skydata:123321@skydata-project-cluster-gm2gy.mongodb.net/skydata-db?retryWrites=true&w=majority',
 		corsURL: process.env.CORS_URL,
         authCookieName: 'x-auth-token'
     }
