d4e02bfb03bc55872f6ba68796baa355c45b5879
PetrovEvgeniy <evgeniypetrov945@gmail.com>
2023-03-02 01:08:06 +0100

    Update corsURL for Render

--- config/config.js
+++ config/config.js
@@ -4,13 +4,13 @@ const config = {
     development: {
         port: process.env.PORT || 9999,
         dbURL: 'mongodb+srv://admin-skydata:123321@skydata-project-cluster-gm2gy.mongodb.net/skydata-db?retryWrites=true&w=majority',
-	    corsURL: 'https://skydata.azurewebsites.net/',
+	    corsURL: 'https://skydata-app.web.app/',
         authCookieName: 'x-auth-token'
     },
     production: {
         port: process.env.PORT,
         dbURL: 'mongodb+srv://admin-skydata:123321@skydata-project-cluster-gm2gy.mongodb.net/skydata-db?retryWrites=true&w=majority',
-		corsURL: process.env.CORS_URL,
+		corsURL: 'https://skydata-app.web.app/',
         authCookieName: 'x-auth-token'
     }
 };
